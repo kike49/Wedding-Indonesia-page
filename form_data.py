07127b3d2e1060049cda48e5f8ff8a63f34851b7
kike49 <enadalesc@hotmail.com>
2024-04-20 20:40:01 +0200

    debugs removed and google fomr auto sumbit implemented

--- form_data.py
+++ form_data.py
@@ -31,7 +31,9 @@ def submit():
 
     return render_template('index.html')
 
+@app.before_first_request
+def create_database():
+  db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+  app.run()
